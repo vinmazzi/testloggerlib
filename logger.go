9a1dceb5653f43904ac66ce52cb9521d92299d05
Vinicius Mazzi <vinicius.mazzi@gmail.com>
2025-05-19 11:56:08 -0300

    Add Middleware

--- logger.go
+++ logger.go
@@ -2,15 +2,31 @@ package testloggerlib
 
 import (
 	"log"
+	"net/http"
 )
 
 type LoggerObj struct {
-	Status   int
-	Endpoint string
-	Method   string
-	URL      string
+	http.ResponseWriter
+	Status int
 }
 
-func LoggerNew(lo LoggerObj) {
-	log.Printf("%s %d %s %s", lo.Method, lo.Status, lo.Endpoint, lo.URL)
+func (lo *LoggerObj) WriteHeader(status int) {
+	lo.Status = status
 }
+
+func Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(wr http.ResponseWriter, r *http.Request) {
+		lo := &LoggerObj{
+			ResponseWriter: wr,
+			Status:         http.StatusOK,
+		}
+
+		next.ServeHTTP(lo, r)
+
+		log.Printf("%s %d %s %s", r.Method, lo.Status, r.URL.Path, r.URL)
+	})
+}
+
+//func LoggerNew(lo LoggerObj) {
+//	log.Printf("%s %d %s %s", lo.Method, lo.Status, lo.Endpoint, lo.URL)
+//}
