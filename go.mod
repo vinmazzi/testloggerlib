f8bf63191e3280eb2535546743fd8fef3ee902b3
Vinicius Mazzi <vinicius.mazzi@gmail.com>
2025-05-18 21:55:18 -0300

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vinmazzi/testloggerlib
+
+go 1.24.3
---
+++ logger/logger.go
@@ -0,0 +1,16 @@
+package logger
+
+import (
+	"log"
+)
+
+type LoggerObj struct {
+	Status   int
+	Endpoint string
+	Method   string
+	URL      string
+}
+
+func LoggerNew(lo LoggerObj) {
+	log.Printf("%s %d %s %s", lo.Method, lo.Status, lo.Endpoint, lo.URL)
+}
